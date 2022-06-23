81a5009cce1b8c74c83c473bdd78506cbdf90f0d
flidov <mrfioden@gmail.com>
2022-06-24 00:15:26 +0300

    check and create 3 FOLDERS and not repeat FILES

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,7 @@ public class Test {
         Scanner sc = new Scanner(System.in);
         GetLine program1 = new GetLine();
         boolean run=true;
+        System.out.println("Выберите \n 1 для ввода строки \n 2 для вывода на экран ");
         while (run) {
             switch (sc.nextInt()) {
                 case 1:
@@ -18,9 +19,6 @@ public class Test {
                     program1.printFileSystem();
                     break;
                 case 3:
-                    program1.createFoldersFileOnLineTest();
-                    break;
-                case 4:
                     run = false;
 
             }
